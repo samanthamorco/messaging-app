ebc262a0d9d91efb78c755c95a991eeab1bdec3d
Samantha Morco <morkotiks@gmail.com>
2017-03-15 13:54:30 -0400

    create chatrooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315155836) do
+ActiveRecord::Schema.define(version: 20170315175218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 names = [Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name, "Sami"]
 names.each do |name|
-  user = User.create(name: name, email: "#{name}@email.com", password: "password", password_confirmation: "password")
+  user = User.create(name: name, email: "#{name.downcase}@email.com", password: "password", password_confirmation: "password")
   3.times do
     Message.create(body: Faker::Hipster.sentence, user_id: user.id, created_at: (1..50).to_a.sample.hours.ago)
   end
