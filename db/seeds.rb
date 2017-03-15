6428a553878f909c27ff695325e3fb6e4b2cf5c3
Samantha Morco <morkotiks@gmail.com>
2017-03-15 12:05:08 -0400

    change to hipster lorem ipsum because why not

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ names = [Faker::Name.first_name, Faker::Name.first_name, Faker::Name.first_name,
 names.each do |name|
   user = User.create(name: name, email: "#{name}@email.com", password: "password", password_confirmation: "password")
   3.times do
-    Message.create(body: Faker::Lorem.paragraph, user_id: user.id, created_at: (1..50).to_a.sample.hours.ago)
+    Message.create(body: Faker::Hipster.sentence, user_id: user.id, created_at: (1..50).to_a.sample.hours.ago)
   end
 end
