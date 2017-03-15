a98df8b879db27b69a4c3d3d439663d3a3e7016f
Samantha Morco <morkotiks@gmail.com>
2017-03-15 12:11:42 -0400

    create messages index and ability to send new message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "messages#index"
+
   get "/signup", to: "users#new"
   post "/users", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+
+  resources :messages, only: [:index, :create]
 end
